faf44a288f708455f45d258b0c3910f897caa764
zero <zero@zero-deb.Zero>
2011-02-16 19:35:45 +0100

    Qt4 start files

---
+++ AddPeSection/AddPeSection.pro
@@ -0,0 +1,16 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-02-16T19:32:57
+#
+#-------------------------------------------------
+
+TARGET = AddPeSection
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
--- AddPeSection/main.cpp
+++ AddPeSection/main.cpp
@@ -1,49 +1,10 @@
-#include <windows.h>
-#include <stdio.h>
+#include <QtGui/QApplication>
+#include "mainwindow.h"
 
-int PrintUse(void);
-int ValidateArgs(int nArgs,char *Args[]);
-
-int main(int argc,char* argv[])
+int main(int argc, char *argv[])
 {
-	if(!ValidateArgs(argc,argv))
-	{
-		PrintUse();
-		return 0;
-	}
-    /* Zero Guay xD*/
-	//TODO
-}
-
-int PrintUse(void)
-{
-	printf("%s%s%s%s%s%s%s","\nAddPeSection, simple tool to add sections to a PE file.\n",
-			"Use:\n\n",
-			"addpesection -n [number] (-name [sectionname] -size [sectionsize]) -flags [RWX] (-v (verbose))\n\n",
-			"n: Number of sections to add.\n",
-			"name: Name of new section(s) (default: '.new[n]').\n",
-			"size: Section(s) size (default: the smallest).\n",
-			"flags: R (Read) W (Write) X (Execute).\n");
-	return 0;
-}
-
-int ValidateArgs(int nArgs,char *Args[])
-{
-	if(nArgs<5) return 0;
-
-	char* lstArgs[]={"-n","-name","-size","-flags","-v"};
-
-	for(int i=1;i<nArgs;i+=2)
-	{
-		int flag=0;
-		for(int n=0;n<5;n++)
-		{
-			if(!lstrcmpA(Args[i],lstArgs[n]))
-			{
-				flag=1;
-			}
-		}
-		if(!flag) return 0;
-	}
-	return 1;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
